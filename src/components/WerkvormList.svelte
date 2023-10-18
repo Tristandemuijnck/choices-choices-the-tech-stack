47ab0250db3de0b8b826a889dc6ae30b5d7f3a34
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-10-18 18:48:25 +0200

    Directus data fetch

---
+++ src/components/WerkvormList.svelte
@@ -0,0 +1,21 @@
+<script>
+    const data = fetch('http://localhost:8055/items/Werkvorm')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+
+    console.log(data)
+
+</script>
+
+<ul>
+    <!-- {#each werkvormen as werkvorm}
+    <li>
+        <h3>{werkvorm.title}</h3>
+    </li>
+    {/each} -->
+</ul>
